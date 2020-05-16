f8dce97ce544ce78c07bdb575e0316153841ef5c
Andy Blight <andy.blight@gmail.com>
2020-05-16 16:25:28 +0100

    Modified ping_server to work with my Python example

--- arduino/nRF905/examples/ping_server/ping_server.ino
+++ arduino/nRF905/examples/ping_server/ping_server.ino
@@ -1,9 +1,8 @@
 /*
- * Project: nRF905 AVR/Arduino Library/Driver (Ping server example)
- * Author: Zak Kemble, contact@zakkemble.co.uk
- * Copyright: (C) 2017 by Zak Kemble
+ * Project: nRF905 AVR/Arduino Library/Driver (Timed Transmit example)
+ * Author: Andy Blight
+ * Copyright: (C) 2020 Andy Blight.
  * License: GNU GPL v3 (see License.txt)
- * Web: http://blog.zakkemble.co.uk/nrf905-avrarduino-librarydriver/
  */
 
 /*
@@ -11,6 +10,7 @@
  *
  * Listen for packets and send them back
  *
+ * 5 -> LED toggles when packet received.
  * 7 -> CE
  * 8 -> PWR
  * 9 -> TXE
@@ -32,6 +32,8 @@
 #define PACKET_OK		1
 #define PACKET_INVALID	2
 
+#define LED_PIN 5
+
 static volatile uint8_t packetStatus;
 
 void NRF905_CB_RXCOMPLETE(void)
@@ -46,18 +48,49 @@ void NRF905_CB_RXINVALID(void)
 	nRF905_standby();
 }
 
+void toggleLed()
+{
+	// Toggle LED
+	static uint8_t ledState = 0;
+	ledState = !ledState;
+	digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+}
+
+void printMsg(uint8_t buffer[])
+{
+	// Print out the message in the buffer.
+	int packet_number = buffer[0];
+	int packet_length = buffer[1];
+	Serial.print(F("Received: "));
+	Serial.print(packet_number);
+	Serial.print(F(", "));
+	Serial.print(packet_length);
+	Serial.print(F(", '"));
+	for (int index = 0; index < packet_length; ++index)
+	{
+		Serial.print((char)buffer[index + 2]);
+	}
+	Serial.print(F("'"));
+	Serial.println();
+}
+
 void setup()
 {
 	Serial.begin(115200);
 	Serial.println(F("Server started"));
 
-	pinMode(A5, OUTPUT); // LED
+	pinMode(LED_PIN, OUTPUT);
+	toggleLed();
 
 	// Start up
 	nRF905_init();
-	
+
 	// Set address of this device
 	nRF905_setListenAddress(RXADDR);
+
+	// Set freqenucy to 434.2MHz.  Band = 0, channel = 118
+	nRF905_setBand(NRF905_BAND_433);
+	nRF905_setChannel(118);
 
 	// Put into receive mode
 	nRF905_RX();
@@ -67,6 +100,7 @@ void loop()
 {
 	static uint32_t pings;
 	static uint32_t invalids;
+	static uint32_t tx_busy;
 
 	Serial.println(F("Waiting for ping..."));
 
@@ -88,23 +122,27 @@ void loop()
 		// Make buffer for data
 		uint8_t buffer[NRF905_MAX_PAYLOAD];
 		nRF905_read(buffer, sizeof(buffer));
+		printMsg(buffer);
+		Serial.println(F("Sending reply..."));
 
-		Serial.println(F("Got ping, sending reply..."));
+		// Delay makes reply reliable.  No delay means no receive.  No idea why!
+		delay(25);
 
 		// Send back the data, once the transmission has completed go into receive mode
-		while(!nRF905_TX(TXADDR, buffer, sizeof(buffer), NRF905_NEXTMODE_RX));
+		bool sent = false;
+		while(!sent)
+		{
+			sent = nRF905_TX(TXADDR, buffer, sizeof(buffer), NRF905_NEXTMODE_RX);
+			if (!sent)
+			{
+				++tx_busy;
+			}
+		}
 
 		Serial.println(F("Reply sent"));
 
-		// Toggle LED
-		static uint8_t ledState;
-		digitalWrite(A5, ledState ? HIGH : LOW);
-		ledState = !ledState;
+		toggleLed();
 
-		// Print out ping contents
-		Serial.print(F("Data from server: "));
-		Serial.write(buffer, sizeof(buffer));
-		Serial.println();
 	}
 
 	Serial.print(F("Totals: "));
@@ -112,5 +150,7 @@ void loop()
 	Serial.print(F(" Pings, "));
 	Serial.print(invalids);
 	Serial.println(F(" Invalid"));
+	Serial.print(tx_busy);
+	Serial.println(F(" tx busy"));
 	Serial.println(F("------"));
 }
